2eab433d4f05de1b27dd62fa113bc96b70f4c98b
RyanSparkc <bluegx08@gmail.com>
2024-12-18 16:47:40 +0800

    新增 圖片和SCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css:[
+    '@/assets/styles/all.scss'
+  ],
+  components:{
+    dirs:[
+      {
+        path:'~/components/global',
+        global:true
+      }
+    ]
+  },
 })
