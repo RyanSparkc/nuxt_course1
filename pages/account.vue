c0ac61eeb9df40cb0974cf575d55b5e0ccaeed21
RyanSparkc <bluegx08@gmail.com>
2024-12-23 14:55:33 +0800

    新增 登入 / 註冊頁面設定

---
+++ pages/account.vue
@@ -0,0 +1,11 @@
+<script setup>
+  definePageMeta({
+    layout: "account-layout",
+  })
+</script>
+
+<template>
+  <NuxtPage />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ pages/account/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+  navigateTo("/account/accountLogin")
+</script>
+
+<template>
+  <div></div>
+</template>
